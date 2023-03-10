5e9bcfb2f3af90da5cea2a6269e8556a5d612891
H605-Jack <77036183+H605-Jack@users.noreply.github.com>
2023-03-10 11:48:27 -0500

    new file:   program/forloop/flp.java

---
+++ program/forloop/flp.java
@@ -0,0 +1,11 @@
+package forloop;
+
+public class flp {
+    public static void main(String[] args) {
+
+        int a = -1;
+        do {
+            System.out.println(a);
+        } while (a != -1);
+    }
+}
