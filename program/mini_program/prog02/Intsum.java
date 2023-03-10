a25e179b71066ad1e918678312ca3998dde846e0
H605-Jack <77036183+H605-Jack@users.noreply.github.com>
2023-03-10 09:55:56 -0500

    new file:   program/mini_program/prog02/Intsum.java

---
+++ program/mini_program/prog02/Intsum.java
@@ -0,0 +1,20 @@
+package mini_program.prog02;
+
+import java.util.Scanner;
+
+public class Intsum {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Please enter a number: ");
+            int entry = scan.nextInt();
+            if (entry == 0) {
+                i = 10;
+            } else {
+                sum += entry;
+            }
+        }
+        System.out.println("The sum of the numbers is "+sum);
+    }
+}
